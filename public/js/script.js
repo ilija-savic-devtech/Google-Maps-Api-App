831712a0429c12c76cdb666aadacaadd2e63e853
ilijasavic <ilija.savic@devtechgroup.com>
2017-10-27 17:04:07 +0200

    First commit

---
+++ public/js/script.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+    var myLatLng = {lat: 45.253342, lng: 19.845922};
+    var markers = [];
+    var polyLines = [];
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: myLatLng,
+        zoom: 15
+    });
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Devtech DOO'
+    });
+
+    function calcDistance(p1, p2) {
+        return (google.maps.geometry.spherical.computeDistanceBetween(p1, p2) / 1000).toFixed(2);
+    }
+
+
+    map.addListener('click', function (event) {
+        var clickedLatLng = {lat: event.latLng.lat(), lng: event.latLng.lng()};
+        var myArray = [
+            myLatLng, clickedLatLng
+        ];
+        var markersAndLines = function () {
+            var marker = new google.maps.Marker({
+                position: event.latLng,
+                map: map
+            });
+            markers.push(marker);
+            var poly = new google.maps.Polyline({
+                path: myArray,
+                strokeColor: '#FF0000',
+                strokeOpacity: 1,
+                strokeWeight: 3,
+                map: map
+            });
+            polyLines.push(poly);
+        };
+
+        function setMapAndPolyOnAll(map) {
+            for (var i = 0; i < markers.length; i++) {
+                markers[i].setMap(map);
+                polyLines[i].setMap(map);
+            }
+        }
+
+        if (markers !== null && polyLines !== null) {
+            setMapAndPolyOnAll(null);
+            markersAndLines();
+        }
+
+        var devtech = new google.maps.LatLng(myLatLng);
+        var clickedPos = new google.maps.LatLng(clickedLatLng);
+        document.getElementById('result').innerHTML = "Distance from Devtech: " + calcDistance(devtech, clickedPos) + " km";
+    });
+
+});
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <div class="container">
+        <div id="map">
+        </div>
+        <br>
+        <center>
+            <h3>
+                <div id="result">
+                </div>
+            </h3>
+        </center>
+    </div>
+
+@endsection
